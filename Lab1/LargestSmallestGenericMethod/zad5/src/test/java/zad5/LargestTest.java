ac1b21be35a4d2ef6de9fd3bf29c3e36020640cb
ppoblocki <patryk.drv@gmail.com>
2017-02-17 13:38:37 +0100

    Zad5-testy

--- Lab1/LargestSmallestGenericMethod/zad5/src/test/java/zad5/LargestTest.java
+++ Lab1/LargestSmallestGenericMethod/zad5/src/test/java/zad5/LargestTest.java
@@ -15,11 +15,77 @@ public class LargestTest
 	}
 	
 	@Test
-	public void findMaxForPositiveIntegers()
+	public void findMaxForPositiveIntegersAsc()
+	{
+		Integer[] intArray = { new Integer(5), new Integer(10), new Integer(15), new Integer(20) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), 20);
+	}
+	
+	@Test
+	public void findMaxForPositiveIntegersDesc()
+	{
+		Integer[] intArray = { new Integer(5), new Integer(4), new Integer(3), new Integer(2) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), 5);
+	}
+	
+	@Test
+	public void findMaxForPositiveIntegersRandom()
 	{
 		Integer[] intArray = { new Integer(5), new Integer(10), new Integer(0), new Integer(1) };
+		assertNotNull(test.max(intArray));
 		assertEquals((int)test.max(intArray), 10);
 	}
+	
+	@Test
+	public void findMaxForNegativeIntegersAsc()
+	{
+		Integer[] intArray = { new Integer(-5), new Integer(-3), new Integer(-1) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), -1);
+	}
+	
+	@Test
+	public void findMaxForNegativeIntegersDesc()
+	{
+		Integer[] intArray = { new Integer(-1), new Integer(-3), new Integer(-5) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), -1);
+	}
+	
+	@Test
+	public void findMaxForNegativeIntegersRandom()
+	{
+		Integer[] intArray = { new Integer(-1), new Integer(-5), new Integer(-2) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), -1);
+	}
+	
+	@Test
+	public void findMaxForIntegers()
+	{
+		Integer[] intArray = { new Integer(-1), new Integer(0), new Integer(1) };
+		assertNotNull(test.max(intArray));
+		assertEquals((int)test.max(intArray), 1);
+	}
+
+	@Test
+	public void findMaxForDouble()
+	{
+		Double[] doubleArray = { new Double(-0.11), new Double(0.0), new Double(1.1), new Double(1.12), new Double(1.11) };
+		assertNotNull(test.max(doubleArray));
+		assertEquals((double)test.max(doubleArray), 1.12, 0);
+	}
+	
+	@Test
+	public void findMaxForDouble2()
+	{
+		Double[] doubleArray = { new Double(1), new Double(0.0), new Double(2), new Double(1.555), new Double(1.55) };
+		assertNotNull(test.max(doubleArray));
+		assertEquals((double)test.max(doubleArray), 2, 0);
+	}
+	
 	
 	@After
 	public void after(){
