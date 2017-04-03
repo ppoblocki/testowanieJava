46946be7d769e82237a569c91e208eab61ad1543
ppoblocki <patryk.drv@gmail.com>
2017-04-03 12:06:27 +0200

    Add lab7 - zad 2

---
+++ lab7/zad2/ExampleJBehave/src/test/java/JBehaveExample/ExampleJBehave/CalculatorStepsRunner2.java
@@ -0,0 +1,30 @@
+package JBehaveExample.ExampleJBehave;
+
+import org.jbehave.core.annotations.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class CalculatorStepsRunner2{
+	
+	private Calculator test;
+	private int inputA;
+	private int inputB;
+	private int output;
+	
+	@Given("I have got created calculator")
+	public void givenNewCalculator(){
+		test = new Calculator();
+	}
+	@When("I will multiply number <inputA> and num <inputB>")
+	public void whenMultyplying(@Named("inputA") int number, @Named("inputB") int num){
+		 test.setLeft(number);
+		 test.setRight(num);
+	}
+	@Then("after multiplying I will get output <output>")
+	public void thenOutput(@Named("output") int output){
+		 assertEquals(output, test.add(test.getLeft(), test.getRight()));
+	}
+}
---
+++ lab7/zad2/ExampleJBehave/stories/calculator_test_runner.story
@@ -0,0 +1,16 @@
+Sample story
+
+Narrative:
+In order to aby pokazać przykład użycia BDD
+As a prowadzący zajęcia
+I want to użyje BDD do kalkulatora
+					 
+Scenario:  Test mnozenia liczb
+Given I have got created calculator
+When I will multiply number <inputA> and num <inputB>
+Then after multiplying I will get output <output>
+
+Examples:     
+|inputA|inputB|output|
+|1|2|3|
+|0|0|0|
